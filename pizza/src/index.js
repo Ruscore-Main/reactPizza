66b20b659722cd62249790362111c8b72845fdf9
Ruscore-Main <oobitovich@mail.ru>
2021-07-09 09:46:32 +0300

    pivot commit

---
+++ pizza/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+import './scss/app.scss'
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
